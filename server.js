05b8c1cb800d98a7bab69e823d6fa2cde7ed7a5e
Aravind cheziyan <arvind.chez@gmail.com>
2020-07-15 15:32:11 +0530

    final product

--- server.js
+++ server.js
@@ -1,78 +1,78 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const shortid = require('shortid');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/chezzy-fashions", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+app.use("/", express.static(__dirname + "/build"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+
+mongoose.connect(
+    process.env.MONGODB_URL || "mongodb://localhost/chezzy-fashions",
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const Product = mongoose.model(
+    "products",
+    new mongoose.Schema({
+        _id: { type: String, default: shortid.generate },
+        title: String,
+        description: String,
+        image: String,
+        price: Number,
+        availableSizes: [String],
+    })
+);
+
+app.get("/api/products", async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
 });
 
-const Product = mongoose.model("products", new mongoose.Schema({
-    _id: {
-        type: String,
-        default: shortid.generate
-    },
-    title: {
-        type: String
-    },
-    description: {
-        type: String
-    },
-    price: {
-        type: String
-    },
-    image: {
-        type: String
-    },
-    availableSizes: [String]
-}));
-
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.send(products);
-    } catch (e) {
-        res.status(500).send();
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    const product = new Product(req.body);
-    const savedProduct = await product.save();
+app.post("/api/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
     res.send(savedProduct);
-})
+});
 
-app.delete('/api/products/:id', async (req, res) => {
-    const product = await Product.findByIdAndDelete(req.params.id)
-    res.send(product);
-})
+app.delete("/api/products/:id", async (req, res) => {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deletedProduct);
+});
 
-const Order = mongoose.model('order', new mongoose.Schema({
-    _id: {
-        type: String,
-        default: shortid.generate
-    },
-    email: String,
-    name: String,
-    address: String,
-    phone: String,
-    total: Number,
-    cartItems: [
+const Order = mongoose.model(
+    "order",
+    new mongoose.Schema(
         {
-            _id: String,
-            title: String,
-            price: Number,
-            count: Number
+            _id: {
+                type: String,
+                default: shortid.generate,
+            },
+            email: String,
+            name: String,
+            phone: String,
+            address: String,
+            total: Number,
+            cartItems: [
+                {
+                    _id: String,
+                    title: String,
+                    price: Number,
+                    count: Number,
+                },
+            ],
         },
-    ],
-}, {
-    timestamps: true
-}))
+        {
+            timestamps: true,
+        }
+    )
+);
 
 app.post("/api/orders", async (req, res) => {
     if (
@@ -99,7 +99,5 @@ app.delete("/api/orders/:id", async (req, res) => {
     res.send(order);
 });
 
-const port = process.env.port || 5000;
-app.listen(port, () => {
-    console.log(`server started listening on port: ${port}`)
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at http://localhost:5000"));
--- src/reducers/product.js
+++ src/reducers/product.js
@@ -1,4 +1,8 @@
-const { FETCH_PRODUCTS, FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } = require("../types");
+import {
+    FETCH_PRODUCTS,
+    FILTER_PRODUCTS_BY_SIZE,
+    ORDER_PRODUCTS_BY_PRICE,
+} from "../types";
 
 export const productsReducer = (state = {}, action) => {
     switch (action.type) {
@@ -6,17 +10,17 @@ export const productsReducer = (state = {}, action) => {
             return {
                 ...state,
                 size: action.payload.size,
-                filteredItems: action.payload.items
-            }
+                filteredItems: action.payload.items,
+            };
         case ORDER_PRODUCTS_BY_PRICE:
             return {
                 ...state,
                 sort: action.payload.sort,
-                filteredItems: action.payload.items
-            }
+                filteredItems: action.payload.items,
+            };
         case FETCH_PRODUCTS:
             return { items: action.payload, filteredItems: action.payload };
         default:
             return state;
     }
-} 
+};
