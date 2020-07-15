f0951fd0dddeab0956770b56a35ad41cb8f7b28e
Aravind cheziyan <arvind.chez@gmail.com>
2020-07-16 01:51:54 +0530

    fixed formatting

--- src/emails/account.js
+++ src/emails/account.js
@@ -20,7 +20,7 @@ const sendWelcomeMail = (order) => {
 const composeEmail = (order) => {
 
     const orderDetails = order.cartItems.map((x) => (
-        `<div>  ${x.count} ${" x "} ${x.title} </div>`
+        `<div style="color: yellow >  ${x.count} ${" x "} ${x.title} </div>`
     ))
 
     return `<div>
@@ -28,25 +28,25 @@ const composeEmail = (order) => {
         <h2>Order Number: ${order._id}</h2>
         <ul>
             <li>
-                <div><strong>Name</strong>: ${order.name}</strong></div>
+                <div><strong>Name</strong>: ${order.name}</div>
             </li>
             <li>
-                <div><strong>Email: ${order.email}</strong></div>
+                <div><strong>Email:</strong> ${order.email}</div>
             </li>
             <li>
-                <div><strong>Phone: ${order.phone}</strong></div>
+                <div><strong>Phone:</strong> ${order.phone}</div>
             </li>
             <li>
-                <div><strong>Address: ${order.address}</strong></div>
+                <div><strong>Address:</strong> ${order.address}</div>
             </li>
             <li>
-                <div><strong>Date: ${convertToAppDate(order.createdAt)}</strong></div>
+                <div><strong>Date:</strong> ${convertToAppDate(order.createdAt)}</div>
             </li>
             <li>
-                <div><strong>Total: ${formatCurrency(order.total)}</strong></div>
+                <div><strong>Total:<span style="color: organge"> ${formatCurrency(order.total)}</span></strong></div>
             </li>
             <li>
-                <div>Cart Items: ${orderDetails}</div>
+                <div><strong>Cart Items:<strong> ${orderDetails}</div>
              </li>
         </ul>
     </div>`
