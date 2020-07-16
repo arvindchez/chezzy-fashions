1278da1ef84313d4a60d6958679f377e90a6d4a5
Aravind cheziyan <arvind.chez@gmail.com>
2020-07-17 02:08:35 +0530

    added search funtionality+dynamic drop down values

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { filterProductsBySize, sortProducts, filterProductsByColor } from "../actions/product";
+import { filterProductsBySize, sortProducts, searchProducts, filterProductsByColor } from "../actions/product";
 import Loading from './Loading';
+
+
+const getUnique = (items, value) => {
+    let type = [...new Set(items.map(item => item[value]))]
+    type = new Set(type.flat(1))
+    return ['All', ...type]
+}
 
 class Filter extends Component {
     render() {
@@ -9,10 +16,10 @@ class Filter extends Component {
             <div> <Loading /></div>
         ) : (
                 <div className="filter">
-                    <div className="filter-result">
+                    <div>
                         {this.props.filteredProducts.length} Products
                     </div>
-                    <div className="filter-sort">
+                    <div>
                         Order{" "}
                         <select
                             value={this.props.sort}
@@ -28,62 +35,65 @@ class Filter extends Component {
                             <option value="highest">Highest</option>
                         </select>
                     </div>
-                    <div className="filter-size">
+                    <div>
                         Size{" "}
                         <select
-                            value={this.props.size}
+                            name="size"
+                            value={this.props.size ? this.props.size : "All"}
                             onChange={(e) =>
                                 this.props.filterProductsBySize(this.props.products, e.target.value)
                             }
-                        >
-                            <option value="">ALL</option>
-                            <option value="Egyméret">Egyméret</option>
-                            <option value="10x10cm">10x10 cm</option>
-                            <option value="15x15cm">15x15 cm</option>
-                            <option value="100x115cm(+-5cm)">100x115 cm (+- 5cm)</option>
-                            <option value="10-13cm">10-13 cm</option>
-                            <option value="13-16cm">13-16 cm</option>
-                            <option value="Kicsi(65x40cmb-első)">Kicsi (65x40 cm belső)</option>
-                            <option value="Nagy(90x45cm-belső)">Nagy (90x45 cm belső)</option>
+                        >{
+                                this.props.products ?
+                                    (
+                                        getUnique(this.props.products, 'availableSizes')
+                                            .map((item, index) => {
+                                                if (item === "All") {
+                                                    return <option value="" key={index}>{item}</option>
+                                                } else {
+                                                    return <option value={item} key={index}>{item}</option>
+                                                }
+                                            })
+
+                                    ) : "All"
+
+                            }
                         </select>
                     </div>
-                    <div className="filter-color">
+                    <div>
                         Colour{" "}
                         <select
-                            value={this.props.color}
+                            name="color"
+                            value={this.props.color ? this.props.color : "All"}
                             onChange={(e) =>
                                 this.props.filterProductsByColor(this.props.products, e.target.value)
                             }>
-                            <option value="">ALL</option>
-                            <option value="fehér">fehér</option>
-                            <option value="ekrü">ekrü</option>
-                            <option value="bézs">bézs</option>
-                            <option value="szürke">szürke</option>
-                            <option value="rose">rose</option>
-                            <option value="porrózsaszín">porrózsaszín</option>
-                            <option value="menta">menta</option>
-                            <option value="Igény-szerint">Igény szerint</option>
-                            <option value="Halvány-menta">Halvány menta</option>
-                            <option value="közép-menta">közép menta</option>
-                            <option value="sötét-menta">sötét menta</option>
-                            <option value="smaragd">smaragd</option>
-                            <option value="sötétkék">sötétkék</option>
-                            <option value="farmerkék">farmerkék</option>
-                            <option value="mályva">mályva</option>
-                            <option value="pasztellrózsaszín">pasztellrózsaszín</option>
-                            <option value="világosszürke">világosszürke</option>
-                            <option value="vanília">vanília</option>
-                            <option value="rózsaszín">rózsaszín</option>
-                            <option value="kék">kék</option>
-                            <option value="világoskék">világoskék</option>
-                            <option value="türkiz">türkiz</option>
-                            <option value="Rose-fehér-szürke">Rose-fehér-szürke</option>
-                            <option value="Menta-türkiz-fehér">Menta-türkiz-fehér</option>
-                            <option value="fehér-bézs-szürke">fehér-bézs-szürke</option>
-                            <option value="Rose-világoskék-fehér">Rose-világoskék-fehér</option>
-                            <option value="Menta-fehér-szürke">Menta-fehér-szürke</option>
-                            <option value="Menta-rose-fehér">Menta-rose-fehér</option>
+                            {
+                                this.props.products ?
+                                    (
+                                        getUnique(this.props.products, 'availableColours')
+                                            .map((item, index) => {
+                                                if (item === "All") {
+                                                    return <option value="" key={index}>{item}</option>
+                                                } else {
+                                                    return <option value={item} key={index}>{item}</option>
+                                                }
+                                            })
+
+                                    ) : "All"
+
+                            }
                         </select>
+                    </div>
+                    <div>
+                        <input className="filter-input"
+                            type="text"
+                            autoComplete="false"
+                            name="search"
+                            placeholder="Search product..."
+                            onKeyUp={(e) =>
+                                this.props.searchProducts(e.target.value)
+                            } />
                     </div>
                 </div>
             );
@@ -100,6 +110,7 @@ export default connect(
     {
         filterProductsBySize,
         sortProducts,
-        filterProductsByColor
+        filterProductsByColor,
+        searchProducts
     }
 )(Filter);
