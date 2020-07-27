7e3012cce2ddd65a27da69bb1568a6652e1c47f7
Aravind cheziyan <arvind.chez@gmail.com>
2020-07-28 03:12:10 +0530

    removed loading message

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import styled from "styled-components";
 import smallLoadingGif from '../../images/gif/small-loading.gif'
-import bigLoadingGif from '../../images/gif/big-loading.gif'
 
 const Loading = () => {
     return (
         <div>
-            <h4>Loading...</h4>
             <Image alt="Loading..." />
         </div>
     )
