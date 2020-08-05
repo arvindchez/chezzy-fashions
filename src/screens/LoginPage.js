75c61b4e958e0faa47795d20c73e728a6b3188d1
Aravind cheziyan <arvind.chez@gmail.com>
2020-08-05 17:49:29 +0530

    login page complete

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import Fade from "react-reveal/Fade";
-
 import { userActions } from '../actions/user';
-import Title from '../components/title/Title';
+import Slider from 'react-slick';
+import { history } from '../helper/history';
+import { alertActions } from '../actions/alert';
 
 function LoginPage(props) {
     const [inputs, setInputs] = useState({
@@ -21,8 +20,31 @@ function LoginPage(props) {
         setInputs(inputs => ({ ...inputs, [name]: value }));
     }
 
+    const handleForgotPasswordSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email && !password) {
+            dispatch(alertActions.error('Please provide email and password'));
+            return;
+        }
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
+
+        if (!email && !password) {
+            dispatch(alertActions.error('Please provide email and password'));
+            return;
+        }
+
+        if (!email) {
+            dispatch(alertActions.error('Please provide email'));
+            return;
+        }
+        if (!password) {
+            dispatch(alertActions.error('Please provice password'));
+            return;
+        }
 
         if (email && password) {
             dispatch(
@@ -34,133 +56,129 @@ function LoginPage(props) {
         }
     }
 
+    var settings = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        autoplay: false,
+        dots: false,
+        infinite: false,
+        draggable: false,
+        swipeToSlide: false
+    };
+
+    function NextArrow(props) {
+        const { onClick, currentSlide } = props;
+        return (
+            currentSlide === 0 && (
+                <div className="slider-btn" onClick={onClick}>
+                    <span className="next position-top right-0 slick-arrow">
+                        Forgot Your Password?
+                </span>
+                </div>
+            )
+        );
+    }
+
+    function PrevArrow(props) {
+        const { onClick, currentSlide } = props;
+        return (
+            currentSlide === 1 && (
+                < div className="slider-btn" onClick={onClick} >
+                    <span className="prev position-top slick-arrow">
+                        Back to Login?
+                </span>
+                </div >
+            )
+        );
+    }
+
+    const redirectToRegister = () => {
+        history.push({
+            pathname: '/register'
+        });
+    }
+
     return (
-        <div>
-            <div class="account-login">
-                <div class="block block-login">
-                    <div class="block-slider">
-                        <ul class="slides" >
-                            <li class="flex-active-slide" >
-                                <div class="block-title">
-                                    <strong><span>Sign In</span></strong>
-                                </div>
-                                <div class="block-content">
-                                    <form
-                                        method="post" id="login-form">
-                                        <input name="form_key" type="hidden" value="oUPrg1iHbSYKnQQE" />
-                                        <ul class="form-list">
-                                            <li>
-                                                <label for="email" class="required">Email Address</label>
-                                                <div class="input-box">
-                                                    <input type="text" name="login[username]" value=""
-                                                        id="email"
-                                                        class="input-text required-entry validate-email"
-                                                        title="Email Address" />
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <label for="pass" class="required">Password</label>
-                                                <div class="input-box">
-                                                    <input type="password" name="login[password]"
-                                                        class="input-text required-entry validate-password"
-                                                        id="pass" title="Password" />
-                                                </div>
-                                            </li>
-                                        </ul>
-
-
-
-                                        <button type="submit" class="button" title="Login" name="send"
-                                            id="send2"><span><span>Login</span></span></button>
-                                        <a href="https://shopper.olegnax.com/customer/account/forgotpassword/"
-                                            class="forgot-password"
-                                            id="forgot-password">Forgot Your Password?</a>
-
-                                    </form>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="block-title">
-                                    <strong><span>Forgot Your Password?</span></strong>
-                                </div>
-                                <div class="block-content">
-                                    <form action="https://shopper.olegnax.com/customer/account/forgotpasswordpost/"
-                                        method="post" id="form-validate">
-                                        <ul class="form-list">
-                                            <li>
-                                                <label for="email" class="required">Email Address</label>
-                                                <div class="input-box">
-                                                    <input type="text" name="email" alt="email"
-                                                        id="email_address" class="input-text required-entry validate-email"
-                                                        value="" />
-                                                </div>
-                                            </li>
-                                        </ul>
-                                        <button type="submit" class="button" title="Submit"><span><span>Submit</span></span>
-                                        </button>
-                                        <a href="https://shopper.olegnax.com/customer/account/forgotpassword/"
-                                            class="forgot-password" id="back-login">Back to Login?</a>
-                                    </form>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="new-users">
-                        <button type="button" title="Register"
-                            class="button invert"
-                            onclick="window.location='https://shopper.olegnax.com/customer/account/create/';"><span>
-                                <span>Register</span></span></button>
+        <div className="main row">
+            <div className="col-main-wrap">
+                <div className="account-login">
+                    <div className="block block-login">
+                        <div className="block-slider">
+                            <ul className="slides" >
+                                <Slider {...settings}>
+                                    <li className="flex-active-slide" >
+                                        <div className="block-title">
+                                            <strong><span>Sign In</span></strong>
+                                        </div>
+                                        <div className="block-content">
+                                            <form onSubmit={handleSubmit} method="post" id="login-form">
+                                                <ul className="form-list">
+                                                    <li>
+                                                        <label htmlFor="email" className="required">Email Address</label>
+                                                        <div className="input-box">
+                                                            <input type="text" name="email" onChange={handleChange}
+                                                                id="email" value={email}
+                                                                className="input-text required-entry validate-email"
+                                                                title="Email Address" />
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <label htmlFor="pass" className="required">Password</label>
+                                                        <div className="input-box">
+                                                            <input type="password" name="password" onChange={handleChange}
+                                                                value={password} className="input-text required-entry validate-password"
+                                                                id="pass" title="Password" />
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                                <button type="submit" className="button" title="Login" name="send"
+                                                    id="send2">
+                                                    {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                                                    <span><span>Login</span></span></button>
+                                            </form>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="block-title">
+                                            <strong><span>Forgot Your Password?</span></strong>
+                                        </div>
+                                        <div className="block-content">
+                                            <form onSubmit={handleForgotPasswordSubmit}
+                                                method="post" id="form-validate">
+                                                <ul className="form-list">
+                                                    <li>
+                                                        <label htmlFor="email" className="required">Email Address</label>
+                                                        <div className="input-box">
+                                                            <input type="text" name="email" alt="email"
+                                                                id="email_address" value={email} onChange={handleChange}
+                                                                className="input-text required-entry validate-email"
+                                                            />
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                                <button type="submit" className="button" title="Submit">
+                                                    <span><span>Submit</span></span>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </li>
+                                </Slider>
+                            </ul>
+                        </div>
+                        <div className="new-users">
+                            <button type="button" title="Register"
+                                className="button invert"
+                                onClick={redirectToRegister}><span>
+                                    <span>Register</span></span></button>
+                        </div>
                     </div>
                 </div>
+
             </div>
-
-
-
-
-
-
-            <Fade right cascade>
-                <Title title={"Login"} />
-                <form onSubmit={handleSubmit}>
-                    <ul className="login-form">
-
-                        <li>
-                            <label>Email:</label>
-                            <input
-                                name="email"
-                                type="email"
-                                required
-                                placeholder="Your email"
-                                onChange={handleChange}
-                            ></input>
-                        </li>
-
-                        <li>
-                            <label>Password:</label>
-                            <input
-                                name="password"
-                                type="password"
-                                required
-                                placeholder="Your password"
-                                onChange={handleChange}
-                            ></input>
-                        </li>
-                        <li>
-                            <div>
-                                <button className="btn btn-sm" type="submit">
-                                    {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>}
-                                    Login
-                                </button>
-                                <div>
-                                    No account yet? <Link to="/register">Register</Link>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </form>
-            </Fade>
-        </div>
-    );
+        </div>);
 }
 
 export { LoginPage };
